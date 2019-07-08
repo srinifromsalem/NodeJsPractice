ee8e06a5d574986c1b23c8bb7d1db503e751f788
srinivasan-s <srinivasan.s@in.ibm.com>
2019-07-08 10:10:24 +0100

    Modified the Parsing Logic

--- LoggerDataParser.js
+++ LoggerDataParser.js
@@ -3,9 +3,47 @@ const fs = require ('fs');
 let manuallyEnabledCount = 0;
 let hourlyDataCount = 0;
 let switchedOnCount = 0;
+let switchedOnManuallyCount = 0;
+
+class HourlyDataTempHumid {
+
+  constructor(dateTimeTemp, humidity, temperature) {
+    this.dateTimeTemp = dateTimeTemp.trim();
+    this.humidity = humidity.trim();
+    this.temperature = temperature.trim();//Degree celcius
+  }
+
+  toString(){
+    return this.dateTimeTemp +" "+this.humidity+" "+this.temperature;
+  }
+
+}
+
+let hourlyData = new Array();
+
+function parseIntoHourlyDataObject(dataString, startingLine){
+  let indexOfHumidity = dataString.indexOf('Humidity');
+  let dateTimeTemp = dataString.substring(startingLine.length, indexOfHumidity);
+  let humidityTemp = dataString.substring( indexOfHumidity,dataString.length);
+  let humidityString = humidityTemp.substring(0,humidityTemp.indexOf('Temp')-1);
+  let humidity = humidityString.split('=')[1].trim();
+  let temperatureString = humidityTemp.substring(humidityTemp.indexOf('Temp'),humidityTemp.length);
+  let temperature = temperatureString.split('=')[1].trim();
+  //console.log("\n"+startingLine);
+  console.log(dataString);
+  //console.log(indexOfHumidity);
+  console.log(dateTimeTemp);
+  console.log(humidity);
+  console.log(temperature);
+
+  return new HourlyDataTempHumid(dateTimeTemp, humidity, temperature);
+  //TODO
+}
 
 function parseLoggerContent(data){
-  var array = data.toString().split("\n");
+
+  let manuallyEnabledFlag = false;
+  let array = data.toString().split("\n");
     for(i in array) {
       if(array[i].length >1){
         //console.log(array[i].length);
@@ -13,18 +51,30 @@ function parseLoggerContent(data){
 
         if(array[i].startsWith("Manually Enabled")){
           manuallyEnabledCount++;
+          manuallyEnabledFlag=true;
+          //console.log(array[i]);
         }else if (array[i].startsWith("Hourly Data")){
           hourlyDataCount++;
-        }else if (array[i].startsWith("Switched ON")){
+          hourlyData.push(parseIntoHourlyDataObject(array[i],'Hourly Data : '));
+        }else if (array[i].startsWith("Switched ON") && !manuallyEnabledFlag){
           switchedOnCount++;
+          parseIntoHourlyDataObject(array[i],'Switched ON at ');
+        }else if (array[i].startsWith("Switched ON") && manuallyEnabledFlag){
+          switchedOnManuallyCount++;
+          parseIntoHourlyDataObject(array[i],'Switched ON at ');
+        }else if (array[i].startsWith("Switched OFF") && manuallyEnabledFlag){
+          parseIntoHourlyDataObject(array[i],'Switched OFF at ');
+          manuallyEnabledFlag=false;
+
         }
-        console.log(array[i]);
+      //  console.log(array[i]);
+
       }
 
     }
 }
 
-fs.readFile('C:\\Dev\\Arduino\\Garden Automation Log Arduiono\\GardenAutomationLogger_RealLife.txt', 'utf-8', (err , data) =>{
+fs.readFile('./GardenAutomationLogger_RealLife.txt', 'utf-8', (err , data) =>{
   if (err)  console.error(err);
   else{
     //console.log(data);
@@ -33,6 +83,10 @@ fs.readFile('C:\\Dev\\Arduino\\Garden Automation Log Arduiono\\GardenAutomationL
     console.log( 'manuallyEnabledCount : '+manuallyEnabledCount );
     console.log( 'hourlyDataCount : '+hourlyDataCount );
     console.log( 'switchedOnCount : '+switchedOnCount );
+    console.log( 'switchedOnManuallyCount : '+switchedOnManuallyCount );
+
+    console.log('hourlyDataArray Length '+hourlyData.length);
+    console.log('hourlyDataArray Sample '+hourlyData[2].toString());
   }
 }
 );
